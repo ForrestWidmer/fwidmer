0e505c9c7e2d4b20b0d3d6dd894650a6c3a911bf
Forrest Widmer <fwid77@gmail.com>
2013-12-26 15:31:46 -0700

    First commit and readme update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fwidmer::Application.config.secret_token = 'f3c4b0582dc0c79b2a0853601625864466c8599cd4dc01a611736f3358c5377b3ba3862a185bbfe426f0f1a0d9307e56e71d77bdc45a01ef0b7976d1fe047a32'
