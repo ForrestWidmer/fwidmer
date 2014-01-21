bf0eb3c71654570ed0ab1f53052c1f0939cae169
Forrest Widmer <fwid77@gmail.com>
2014-01-21 03:24:24 -0700

    image precompiling issues with heroku, debugging.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Fwidmer::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.assets.initialize_on_precompile = false
   config.action_mailer.default_url_options = { :host => 'fwidmer.herokuapp.com' }  
   config.assets.precompile += %w( vendor/modernizr.js )
   config.assets.precompile += %w( custom.css.scss )
