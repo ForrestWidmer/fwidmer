43ebdcc413d6282a227d90317fb271cd6cc8ee63
ForrestWidmer <forrest.widmer@gmail.com>
2014-03-08 18:56:36 -0700

    Disabling registrations.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,20 @@
 #Comment out bottom portion and uncomment top to prevent registrations.
 
-#class RegistrationsController < Devise::RegistrationsController
-#  def new
-#    flash[:notice] = "Registrations are not open."
-#    redirect_to root_path
-#  end
-
-#  def create
-#    flash[:notice] = "Registrations are not open."
-#    redirect_to root_path
-#  end
-#end
-
 class RegistrationsController < Devise::RegistrationsController
-  protected 
-  def after_update_path_for(resource)
-    edit_user_registration_path(resource)
+  def new
+    flash[:notice] = "Registrations are not open."
+    redirect_to root_path
+  end
+
+  def create
+    flash[:notice] = "Registrations are not open."
+    redirect_to root_path
   end
 end
+
+#class RegistrationsController < Devise::RegistrationsController
+#  protected 
+#  def after_update_path_for(resource)
+#    edit_user_registration_path(resource)
+#  end
+#end
